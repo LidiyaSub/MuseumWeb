a36b0ead24c7c416402e360e757348302847416b
LidiyaSub <lidiya.subbotina@gmail.com>
2015-12-09 22:55:21 +0200

    added hibernate.cfg.xml, changed pom.xml, added GeneralDao, GeneralDaoImpl, InitSessionFactory

---
+++ src/main/java/com/softserveinc/edu/ita/dao/impl/GeneralDaoImpl.java
@@ -0,0 +1,81 @@
+package com.softserveinc.edu.ita.dao.impl;
+
+import java.lang.reflect.ParameterizedType;
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+
+import com.softserveinc.edu.ita.dao.GeneralDao;
+import com.softserveinc.edu.ita.init.InitSessionFactory;
+
+public abstract class GeneralDaoImpl<E, N extends Number> implements
+		GeneralDao<E, N> {
+
+	protected Class<E> entityClass;
+
+	@SuppressWarnings("unchecked")
+	public GeneralDaoImpl() {
+		entityClass = (Class<E>) ((ParameterizedType) getClass()
+				.getGenericSuperclass()).getActualTypeArguments()[0];
+	}
+
+	SessionFactory sessionFactory = InitSessionFactory.getSessionFactory();
+
+	public void closeSessionFactory() {
+		if (sessionFactory != null)
+			sessionFactory.close();
+	}
+
+	public void save(E entity) {
+		Session session = sessionFactory.openSession();
+		session.beginTransaction();
+		session.save(entity);
+		session.getTransaction().commit();
+		session.close();
+
+	}
+
+	public void update(E entity) {
+		Session session = sessionFactory.openSession();
+		session.beginTransaction();
+		session.update(entity);
+		session.getTransaction().commit();
+		session.close();
+
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<E> getAll() {
+		Session session = sessionFactory.openSession();
+		session.beginTransaction();
+		List<E> entities;
+		entities = session.createCriteria(entityClass).list();
+		session.getTransaction().commit();
+		session.close();
+		return entities;
+	}
+
+	public void delete(E entity) {
+		Session session = sessionFactory.openSession();
+		session.beginTransaction();
+		session.delete(entity);
+		session.getTransaction().commit();
+		session.close();
+
+	}
+
+	@SuppressWarnings("unchecked")
+	public E findOneById(Number id) {
+		Session session = sessionFactory.openSession();
+		session.beginTransaction();
+		E entity;
+		entity = (E) session.createCriteria(entityClass).uniqueResult();
+		return entity;
+	}
+
+	public Class<E> getEntityClass() {
+		return entityClass;
+	}
+
+}
