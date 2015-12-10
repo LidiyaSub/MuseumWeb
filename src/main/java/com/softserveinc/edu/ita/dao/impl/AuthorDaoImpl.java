968bd2e51406ae19d41e6eb98cde736e58061794
kovalartem <kovaltommy@gmail.com>
2015-12-10 15:53:11 +0200

    Updated dependencies in order to support Spring

--- src/main/java/com/softserveinc/edu/ita/dao/impl/AuthorDaoImpl.java
+++ src/main/java/com/softserveinc/edu/ita/dao/impl/AuthorDaoImpl.java
@@ -1,8 +1,11 @@
 package com.softserveinc.edu.ita.dao.impl;
 
+import org.springframework.stereotype.Repository;
+
 import com.softserveinc.edu.ita.dao.AuthorDao;
 import com.softserveinc.edu.ita.entity.Author;
 
+@Repository
 public class AuthorDaoImpl extends GeneralDaoImpl<Author, Long> implements AuthorDao {
 
 }
