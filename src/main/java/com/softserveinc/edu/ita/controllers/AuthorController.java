4d6aa16ca8b22685ad98987dd3cc12a3b3c6d460
LidiyaSub <lidiya.subbotina@gmail.com>
2015-12-11 15:23:30 +0200

    changes in pom.xml, data.xml

--- src/main/java/com/softserveinc/edu/ita/controllers/AuthorController.java
+++ src/main/java/com/softserveinc/edu/ita/controllers/AuthorController.java
@@ -13,24 +13,28 @@ import com.softserveinc.edu.ita.entity.Author;
 
 @Controller
 public class AuthorController {
-	@Autowired
-	AuthorService authorService;
-	
-	@RequestMapping("/showAllAuthors")
-	public String getAllAuthors(Model model){
-		List<Author> authors = authorService.getAll();
-		model.addAttribute("allAuthors", authors);
-		return "show-authors";
+	@RequestMapping(value = "/")
+	public String showInitialPage() {
+		return "index";
 	}
-	@RequestMapping("/createAuthor")
-	public String addAuthor(Model model){
-		model.addAttribute("author", new Author());
-		return "addNewAuthor";
-	}
-	
-	@RequestMapping(value = "/saveAuthor", method = RequestMethod.POST)
-	public String saveAuthorToDB(@ModelAttribute("author")Author author){
-		authorService.save(author);
-		return "redirect:/show-authors?message=true";
-	}
+	// @Autowired
+	// AuthorService authorService;
+	//
+	// @RequestMapping("/showAllAuthors")
+	// public String getAllAuthors(Model model){
+	// List<Author> authors = authorService.getAll();
+	// model.addAttribute("allAuthors", authors);
+	// return "show-authors";
+	// }
+	// @RequestMapping("/createAuthor")
+	// public String addAuthor(Model model){
+	// model.addAttribute("author", new Author());
+	// return "addNewAuthor";
+	// }
+	//
+	// @RequestMapping(value = "/saveAuthor", method = RequestMethod.POST)
+	// public String saveAuthorToDB(@ModelAttribute("author")Author author){
+	// authorService.save(author);
+	// return "redirect:/show-authors?message=true";
+	// }
 }
